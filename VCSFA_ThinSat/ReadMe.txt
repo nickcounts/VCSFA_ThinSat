c3e02f200a3ddb0243eada5e4df6f29774ac4f3c
Nick Counts <nick.counts@gmail.com>
2018-06-11 21:56:56 -0400

    Initial Commit

---
+++ VCSFA_ThinSat/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  VCSFA_ThinSat
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project VCSFA_ThinSat
+  Created by Nicholas Counts on 6/11/18
+  Copyright © 2018 Counts Engineering
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ VCSFA_ThinSat/VCSFA_ThinSat.ino
@@ -0,0 +1,66 @@
+//
+// VCSFA_ThinSat
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Nicholas Counts
+// 				Counts Engineering
+//
+// Date			6/11/18 9:56 PM
+// Version		<#version#>
+//
+// Copyright	© Nicholas Counts, 2018
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
